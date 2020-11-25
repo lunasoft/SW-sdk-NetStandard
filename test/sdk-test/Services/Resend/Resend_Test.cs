35ef19546162b4e5e9b52860ce12d4eafb6b7d9d
Juan Gamez <11juan94@gmail.com>
2020-11-25 14:01:41 -0600

    SMARTER-2491
    Se actualiza servicio de timbrado V4 con parametro opcional de email y customId.
    
    Se actualizan CSD de test y pequeños errores.

--- test/sdk-test/Services/Resend/Resend_Test.cs
+++ test/sdk-test/Services/Resend/Resend_Test.cs
@@ -1,8 +1,4 @@
-﻿using sw_sdk.Services.Resend;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Test_SW.Helpers;
 using Xunit;
 
@@ -15,8 +11,8 @@ namespace sdk_test.Services.Resend
         {
             var build = new BuildSettings();
             sw_sdk.Services.Resend.Resend resend = new sw_sdk.Services.Resend.Resend(build.Url, build.UrlSWServices, build.User, build.Password);
-            var responseResend = await resend.ResendEmailAsync("d48e3be0-14b5-479c-8499-6a76df824ce0", "someemail@some.com");
-            Assert.True(responseResend.data != null && responseResend.status == "success");
+            var responseResend = await resend.ResendEmailAsync("3f946ad4-8a35-4067-8dc7-5227a80f6938", "someemail@some.com");
+            Assert.True(responseResend != null && responseResend.data != null && responseResend.status == "success");
         }
     }
 }
