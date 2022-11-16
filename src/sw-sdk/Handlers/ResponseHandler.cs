74df90cfd5393c8df97962b14781266678cde35f
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2022-11-16 17:36:28 -0600

    Update Response Handlers - Se agregan ResponseHandler y ResponseHandler Extended - ResponseHandler sustituye a todos los handlers que existian en los servicios. - ResponseHandlerExtended contiene metodos para retornar error object del tipo segun sea especfiicado.

---
+++ src/sw-sdk/Handlers/ResponseHandler.cs
@@ -0,0 +1,141 @@
+﻿using SW.Entities;
+using SW.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using SW.Services.Stamp;
+
+namespace SW.Handlers
+{
+    internal class ResponseHandler<T>
+        where T : Response, new()
+    {
+        private ResponseHandlerExtended<T> _handler;
+        public readonly string _xmlOriginal;
+        public ResponseHandler() 
+        {
+            _handler = new ResponseHandlerExtended<T>();
+        }
+        public ResponseHandler(string xmlOriginal)
+        {
+            _handler = new ResponseHandlerExtended<T>();
+            _xmlOriginal = xmlOriginal;
+        }
+        public virtual async Task<T> GetPostResponseAsync(string url, string path, Dictionary<string, string> headers, HttpContent content, HttpClientHandler proxy)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient(proxy))
+                {
+                    client.BaseAddress = new Uri(url);
+                    foreach (var header in headers)
+                    {
+                        client.DefaultRequestHeaders.Add(header.Key, header.Value);
+                    }
+                    var result = await client.PostAsync(path, content);
+                    return await _handler.TryGetResponseAsync(result);
+                }
+            }
+            catch (HttpRequestException wex)
+            {
+                return _handler.GetExceptionResponse(wex);
+            }
+        }
+        public virtual async Task<T> GetPostResponseAsync(string url, Dictionary<string, string> headers, string path, HttpClientHandler proxy)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient(proxy))
+                {
+                    foreach (var header in headers)
+                    {
+                        client.DefaultRequestHeaders.Add(header.Key, header.Value);
+                    }
+                    client.BaseAddress = new Uri(url);
+                    var result = await client.PostAsync(path, null);
+                    return await _handler.TryGetResponseAsync(result);
+                }
+            }
+            catch (HttpRequestException wex)
+            {
+                return _handler.GetExceptionResponse(wex);
+            }
+        }
+
+        public virtual async Task<T> GetResponseAsync(string url, Dictionary<string, string> headers, string path, HttpClientHandler proxy)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient(proxy))
+                {
+                    foreach (var header in headers)
+                    {
+                        client.DefaultRequestHeaders.Add(header.Key, header.Value);
+                    }
+                    client.BaseAddress = new Uri(url);
+                    var result = await client.GetAsync(path);
+                    return await _handler.TryGetResponseAsync(result);
+                }
+            }
+            catch (HttpRequestException wex)
+            {
+                return _handler.GetExceptionResponse(wex);
+            }
+        }
+        
+        internal virtual string GetCfdiData(Response response, string cfdi, bool isb64)
+        {
+            try
+            {
+
+                return XmlUtils.AddAddenda(_xmlOriginal, cfdi, isb64);
+
+            }
+            catch (Exception)
+            {
+            }
+            return cfdi;
+        }
+        internal virtual bool Has307AndAddenda(Response response, Data_CFDI data)
+        {
+            try
+            {
+                if (response.status == "error" &&
+               (response.message != null && response.message.Trim().ToLower().Replace(".", "").Contains("307 el comprobante contiene un timbre previo"))
+               && (data != null && !string.IsNullOrEmpty(data.cfdi)))
+                {
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+            }
+            return false;
+        }
+        internal virtual bool Has307AndAddenda(Response response, Data_CFDI_TFD data)
+        {
+            try
+            {
+                if (response.status == "error" &&
+               (response.message != null && response.message.Trim().ToLower().Replace(".", "").Contains("307 el comprobante contiene un timbre previo"))
+               && (data != null && !string.IsNullOrEmpty(data.cfdi)))
+                {
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+            }
+            return false;
+        }
+        public T HandleException(Exception ex)
+        {
+            return _handler.GetExceptionResponse(ex);
+        }
+    }
+}
---
+++ src/sw-sdk/Handlers/ResponseHandlerExtended.cs
@@ -0,0 +1,56 @@
+﻿using SW.Entities;
+using System;
+using System.Net.Http;
+using System.Net;
+using System.Threading.Tasks;
+using SW.Helpers;
+
+namespace SW.Handlers
+{
+    internal class ResponseHandlerExtended<T> where T : Response, new()
+    {
+        internal virtual async Task<T> TryGetResponseAsync(HttpResponseMessage response)
+        {
+            try
+            {
+                if (response.StatusCode == HttpStatusCode.OK || response.StatusCode == HttpStatusCode.BadRequest)
+                {
+                    return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(await response.Content.ReadAsStringAsync());
+                }
+                else
+                    return GetExceptionResponse(response);
+            }
+            catch
+            {
+                return GetExceptionResponse(response);
+            }
+        }
+        internal T GetExceptionResponse(HttpRequestException ex)
+        {
+            return new T()
+            {
+                message = ex.Message,
+                status = "error",
+                messageDetail = ex.StackTrace
+            };
+        }
+        internal T GetExceptionResponse(Exception ex)
+        {
+            return new T()
+            {
+                status = "error",
+                message = ex.Message,
+                messageDetail = ResponseHelper.GetErrorDetail(ex)
+            };
+        }
+        private T GetExceptionResponse(HttpResponseMessage response)
+        {
+            return new T()
+            {
+                message = ((int)response.StatusCode).ToString(),
+                status = "error",
+                messageDetail = response.ReasonPhrase
+            };
+        }
+    }
+}
