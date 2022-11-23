ff3a42ab46163fe094d679d279196477972cfdb7
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2022-11-23 12:12:53 -0600

    Resend Service - Se agrega servicio de resend.

--- src/sw-sdk/Services/Resend/Resend.cs
+++ src/sw-sdk/Services/Resend/Resend.cs
@@ -1,16 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
 
-namespace sw_sdk.Services.Resend
+namespace SW.Services.Resend
 {
-    public class Resend : BaseResend
+    public class Resend : ResendService
     {
-        public Resend(string urlAuth, string urlApi, string user, string password, int proxyPort = 0, string proxy = null) : base(urlAuth, urlApi, user, password, "pdf", proxy, proxyPort)
+        /// <summary>
+        /// Crear una instancia de la clase Resend.
+        /// </summary>
+        /// <param name="urlApi"></param>
+        /// <param name="url"></param>
+        /// <param name="user"></param>
+        /// <param name="password"></param>
+        /// <param name="proxyPort"></param>
+        /// <param name="proxy"></param>
+        public Resend(string urlApi, string url, string user, string password, int proxyPort = 0, string proxy = null)
+            : base(urlApi, url, user, password, proxy, proxyPort)
         {
         }
-        public Resend(string urlApi, string token, int proxyPort = 0, string proxy = null) : base(urlApi, urlApi, token, "pdf", proxy, proxyPort)
+        /// <summary>
+        /// Crear una instancia de la clase Resend.
+        /// </summary>
+        /// <param name="urlApi"></param>
+        /// <param name="token"></param>
+        /// <param name="proxyPort"></param>
+        /// <param name="proxy"></param>
+        public Resend(string urlApi, string token, int proxyPort = 0, string proxy = null)
+            : base(urlApi, token, proxy, proxyPort)
         {
+        }
+        /// <summary>
+        /// Servicio para realizar el reenvío del XML y/o PDF a los correos especificados. El PDF se envia si fue generado en el proceso de timbrado.
+        /// </summary>
+        /// <param name="uuid">Folio del comprobante timbrado.</param>
+        /// <param name="email">Correo que se hará el reenvío. (Max. 5).</param>
+        /// <returns>ResendResponse</returns>
+        public async Task<ResendResponse> ResendEmailAsync(Guid uuid, string email)
+        {
+            return await ResendEmailServiceAsync(uuid, new[] { email });
+        }
+        /// <summary>
+        /// Servicio para realizar el reenvío del XML y/o PDF a los correos especificados. El PDF se envia si fue generado en el proceso de timbrado.
+        /// </summary>
+        /// <param name="uuid">Folio del comprobante timbrado.</param>
+        /// <param name="email">Listado de correos a los que se hará el reenvío. (Max. 5).</param>
+        /// <returns>ResendResponse</returns>
+        public async Task<ResendResponse> ResendEmailAsync(Guid uuid, string[] email)
+        {
+            return await ResendEmailServiceAsync(uuid, email);
         }
     }
 }
--- src/sw-sdk/Services/Resend/ResendRequest.cs
+++ src/sw-sdk/Services/Resend/ResendRequest.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Runtime.Serialization;
-using System.Text;
+﻿using System.Runtime.Serialization;
 
-namespace sw_sdk.Services.Resend
+namespace SW.Services.Resend
 {
     [DataContract]
     public class ResendRequest
