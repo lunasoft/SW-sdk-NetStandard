55b009c52eedf230e37e5f8bc8bd941698e1e985
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2023-01-04 17:30:41 -0600

    Add Csd Service UT

--- test/sdk-test/Services/Csd/Csd_Test.cs
+++ test/sdk-test/Services/Csd/Csd_Test.cs
@@ -2,26 +2,132 @@
 using SW.Services.Csd;
 using Xunit;
 using System.Threading.Tasks;
+using System;
+using System.Linq;
 
 namespace Test_SW_sdk.Services.Csd
 {
     public class Csd_Test
     {
-        [Fact]
-        public async Task Csd_Test_UploadCsdAsync()
+        private readonly BuildSettings _build;
+        public Csd_Test()
         {
-            var build = new BuildSettings();
-            CsdUtils csd = new CsdUtils(build.Url, build.User, build.Password);
-            var response = await csd.UploadMyCsdAsync(build.Cer, build.Key, build.CerPassword, "stamp", true);
-            Assert.True(response.data != null && response.status == "success");
+            _build = new BuildSettings();
         }
         [Fact]
-        public async Task Csd_Test_UploadCsd_EmptyCsdAsync()
+        public async Task Csd_Test_UploadCsdAsync_Success()
         {
-            var build = new BuildSettings();
-            CsdUtils csd = new CsdUtils(build.Url, build.User, build.Password);
-            var response = await csd.UploadMyCsdAsync("", build.Key, build.CerPassword, "stamp", true);
-            Assert.True(response.message == "El certificado o llave privada vienen vacios" && response.status == "error");
+            CsdUtils csd = new CsdUtils(_build.Url, _build.User, _build.Password);
+            var response = await csd.UploadCsdAsync(_build.Cer, _build.Key, _build.CerPassword);
+            Assert.True(response.status == "success");
+            Assert.NotNull(response.data);
+        }
+        [Fact]
+        public async Task Csd_Test_Auth_UploadCsdAsync_Success()
+        {
+            CsdUtils csd = new CsdUtils(_build.Url, _build.Token);
+            var response = await csd.UploadCsdAsync(_build.Cer, _build.Key, _build.CerPassword);
+            Assert.True(response.status == "success");
+            Assert.NotNull(response.data);
+        }
+        [Fact]
+        public async Task Csd_Test_UploadCsd_EmptyCsdAsync_Error()
+        {
+            CsdUtils csd = new CsdUtils(_build.Url, _build.User, _build.Password);
+            var response = await csd.UploadCsdAsync("", _build.Key, _build.CerPassword);
+            Assert.True(response.status.Equals("error"));
+            Assert.True(response.message.Equals("El certificado o llave privada vienen vacios"));
+        }
+        [Fact]
+        public async Task Csd_Test_UploadCsd_InvalidPassword_Error()
+        {
+            CsdUtils csd = new CsdUtils(_build.Url, _build.Token);
+            var response = await csd.UploadCsdAsync(_build.Cer, _build.Key, "password");
+            Assert.True(response.status.Equals("error"));
+            Assert.True(!String.IsNullOrEmpty(response.message));
+            Assert.True(!String.IsNullOrEmpty(response.messageDetail));
+        }
+        [Fact]
+        public async Task Csd_Test_GetAllCsd_Success()
+        {
+            CsdUtils csd = new CsdUtils(_build.Url, _build.Token);
+            var response = await csd.GetAllCsdAsync();
+            Assert.True(response.status.Equals("success"));
+            Assert.NotNull(response.data);
+            Assert.True(response.data.Count > 0);
+            Assert.True(!String.IsNullOrEmpty(response.data.First().valid_to));
+            Assert.True(!String.IsNullOrEmpty(response.data.First().valid_from));
+            Assert.True(!String.IsNullOrEmpty(response.data.First().issuer_rfc));
+            Assert.True(!String.IsNullOrEmpty(response.data.First().issuer_business_name));
+            Assert.True(!String.IsNullOrEmpty(response.data.First().certificate_number));
+            Assert.True(!String.IsNullOrEmpty(response.data.First().certificate_type));
+            Assert.True(!String.IsNullOrEmpty(response.data.First().csd_certificate));
+        }
+        [Fact]
+        public async Task Csd_Test_GetAllCsdByRfc_Success()
+        {
+            CsdUtils csd = new CsdUtils(_build.Url, _build.Token);
+            var response = await csd.GetAllCsdAsync(_build.Rfc);
+            Assert.True(response.status.Equals("success"));
+            Assert.NotNull(response.data);
+            Assert.True(response.data.Count > 0);
+            Assert.True(!String.IsNullOrEmpty(response.data.First().valid_to));
+            Assert.True(!String.IsNullOrEmpty(response.data.First().valid_from));
+            Assert.True(!String.IsNullOrEmpty(response.data.First().issuer_rfc));
+            Assert.True(!String.IsNullOrEmpty(response.data.First().issuer_business_name));
+            Assert.True(!String.IsNullOrEmpty(response.data.First().certificate_number));
+            Assert.True(!String.IsNullOrEmpty(response.data.First().certificate_type));
+            Assert.True(!String.IsNullOrEmpty(response.data.First().csd_certificate));
+        }
+        [Fact]
+        public async Task Csd_Test_GetAllCsdByRfc_Error()
+        {
+            CsdUtils csd = new CsdUtils(_build.Url, _build.Token);
+            var response = await csd.GetAllCsdAsync("MYRFC");
+            Assert.True(response.status.Equals("error"));
+            Assert.True(!String.IsNullOrEmpty(response.message));
+            Assert.True(!String.IsNullOrEmpty(response.messageDetail));
+        }
+        [Fact]
+        public async Task Csd_Test_GetCsd_Success()
+        {
+            CsdUtils csd = new CsdUtils(_build.Url, _build.Token);
+            var response = await csd.GetCsdAsync("30001000000400002434");
+            Assert.True(response.status.Equals("success"));
+            Assert.NotNull(response.data);
+            Assert.True(!String.IsNullOrEmpty(response.data.valid_to));
+            Assert.True(!String.IsNullOrEmpty(response.data.valid_from));
+            Assert.True(!String.IsNullOrEmpty(response.data.issuer_rfc));
+            Assert.True(!String.IsNullOrEmpty(response.data.issuer_business_name));
+            Assert.True(!String.IsNullOrEmpty(response.data.certificate_number));
+            Assert.True(!String.IsNullOrEmpty(response.data.certificate_type));
+            Assert.True(!String.IsNullOrEmpty(response.data.csd_certificate));
+        }
+        [Fact]
+        public async Task Csd_Test_GetCsd_Error()
+        {
+            CsdUtils csd = new CsdUtils(_build.Url, _build.Token);
+            var response = await csd.GetCsdAsync("MYNOCER");
+            Assert.True(response.status.Equals("error"));
+            Assert.True(!String.IsNullOrEmpty(response.message));
+            Assert.True(!String.IsNullOrEmpty(response.messageDetail));
+        }
+        [Fact]
+        public async Task Csd_Test_DeleteCsdAsync_Success()
+        {
+            CsdUtils csd = new CsdUtils(_build.Url, _build.User, _build.Password);
+            var response = await csd.DeleteCsdAsync("30001000000400002434");
+            Assert.True(response.status == "success");
+            Assert.NotNull(response.data);
+        }
+        [Fact]
+        public async Task Csd_Test_DeleteCsdAsync_Error()
+        {
+            CsdUtils csd = new CsdUtils(_build.Url, _build.Token);
+            var response = await csd.DeleteCsdAsync("1234567890");
+            Assert.True(response.status.Equals("error"));
+            Assert.True(!String.IsNullOrEmpty(response.message));
+            Assert.True(!String.IsNullOrEmpty(response.messageDetail));
         }
     }
 }
