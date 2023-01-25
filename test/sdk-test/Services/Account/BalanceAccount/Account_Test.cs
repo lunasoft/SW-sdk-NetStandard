848fecb8e36020d1377c27a1e23e473c26773e7d
SW-2017-04\Soporte2 <marifer.mares@sw.com.mx>
2023-01-24 19:16:11 -0600

    Reorganizacion de UT de BalanceAccount

---
+++ test/sdk-test/Services/Account/BalanceAccount/Account_Test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using SW.Services.Account;
+using Test_SW.Helpers;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Test_SW.Services.Account_Test
+{
+    public class Account_Test_45
+    {
+
+        [Fact]
+        public async Task Account_Test_45_ConsultaDeSaldoByUserAsync()
+        {
+            var build = new BuildSettings();
+            BalanceAccount account = new BalanceAccount("http://services.test.sw.com.mx", build.User, build.Password);
+            AccountResponse response = await account.ConsultarSaldoAsync();
+            if(response.status != "error")
+            {
+                //saldo timbres
+                Console.WriteLine(response.data.saldoTimbres);
+                //timbres utilizados
+                Console.WriteLine(response.data.timbresUtilizados);
+                //En caso de tener timbres infinitos (para cuentas hijo)
+                Console.WriteLine(response.data.unlimited);
+            }
+            else
+            {
+                //errores
+                Console.WriteLine(response.message);
+                Console.WriteLine(response.messageDetail);
+            }
+            Assert.True(response.status == "success", response.messageDetail);
+        }
+        [Fact]
+        public async Task Account_Test_45_ConsultaDeSaldoByTokenAsync()
+        {
+            var build = new BuildSettings();
+            BalanceAccount account = new BalanceAccount(build.Url, build.Token);
+            var response = await account.ConsultarSaldoAsync();
+            Assert.True(response.status == "success", response.messageDetail);
+        }
+    }
+}
