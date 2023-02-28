05f08deb2c83e06c628aaffd2ea6ec436fecb493
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2023-02-28 13:41:30 -0600

    Add enum account user

---
+++ src/sw-sdk/Helpers/EnumAccountUser.cs
@@ -0,0 +1,15 @@
+﻿
+namespace SW.Helpers
+{
+    public enum AccountUserProfile
+    {
+        Distribuidor = 2,
+        Hijo = 3
+    }
+    internal enum AccountUserAction
+    {
+        Add,
+        Update,
+        Delete
+    }
+}
