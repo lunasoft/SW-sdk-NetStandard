21b36a34042ee5887d3352d7097cc340651dc698
Fernando Carrillo <108951984+FerCarrilloM@users.noreply.github.com>
2025-08-11 13:44:58 -0600

    Se agregan UT

--- test/sdk-test/Services/StampRetention/StampRetention_Test.cs
+++ test/sdk-test/Services/StampRetention/StampRetention_Test.cs
@@ -6,6 +6,7 @@ using Test_SW.Helpers;
 using Xunit;
 using System.IO;
 using Test_SW.Helper;
+using SW.Services.Stamp;
 
 namespace Test_SW.Services.StampRetention_Test
 {
@@ -31,6 +32,42 @@ namespace Test_SW.Services.StampRetention_Test
             var response = (StampRetentionResponseV3)await StampRetention.TimbrarV3Async(xmlRetention);
             CustomAssert.SuccessResponse(response, response.Data);
             Assert.True(!string.IsNullOrEmpty(response.Data.Retencion), "El resultado Data.Retention viene vacio.");
+        }
+
+        [Fact]
+        public async Task StampRetention_Test_ValidateFormatTokenAsync()
+        {
+            var build = new BuildSettings();
+            StampRetention StampRetention = new StampRetention(build.Url, build.Token + ".");
+            var xmlRetention = GetXmlRetention(build);
+            var response = await StampRetention.TimbrarV3Async(xmlRetention);
+            CustomAssert.ErrorResponse(response);
+            Assert.Contains("El token debe contener 3 partes", response.Message);
+            Assert.True(string.IsNullOrEmpty(response.MessageDetail));
+        }
+
+        [Fact]
+        public async Task StampRetention_Test_ValidateEmptyXMLAsync()
+        {
+            var build = new BuildSettings();
+            StampRetention StampRetention = new StampRetention(build.Url, build.Token);
+            var response = await StampRetention.TimbrarV3Async(String.Empty);
+            CustomAssert.ErrorResponse(response);
+            Assert.Equal("301 - La estructura del comprobante es incorrecta.", response.Message);
+            Assert.True(string.IsNullOrEmpty(response.MessageDetail));
+        }
+
+        [Fact]
+        public async Task StampRetention_Test_StampRetentionXMLV3_XMLDuplicado_Error()
+        {
+            var build = new BuildSettings();
+            StampRetention StampRetention = new StampRetention(build.Url, build.Token);
+            var xmlRetention = GetXmlRetention(build);
+            var response = await StampRetention.TimbrarV3Async(xmlRetention);
+            CustomAssert.SuccessResponse(response, response.Data);
+            response = await StampRetention.TimbrarV3Async(xmlRetention);
+            CustomAssert.ErrorResponse(response);
+            Assert.True(response.Message == "307. El comprobante contiene un timbre previo.");
         }
 
         private string GetXmlRetention(BuildSettings build, string fileName = null)
